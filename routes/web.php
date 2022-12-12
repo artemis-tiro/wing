2921babdcb8ac82666dfc71ba250a8aad5b4d975
ogura <harley0.love@gmail.com>
2022-12-12 17:23:45 +0900

    '2022/12/12/ogura'

--- routes/web.php
+++ routes/web.php
@@ -66,6 +66,7 @@ Route::middleware(['auth:sanctum', 'verified'])->any('/i/{miseId}/{therapistId}/
 Route::middleware(['auth:sanctum', 'verified'])->any('/i/{miseId}/{therapistId}/kyuryo', "App\Http\Controllers\InputController@kyuryo");
 Route::middleware(['auth:sanctum', 'verified'])->any('/i/{miseId}/{therapistId}/calculation', "App\Http\Controllers\InputController@calculation");
 Route::middleware(['auth:sanctum', 'verified'])->any('/i/{miseId}/{therapistId}/{id}/del', "App\Http\Controllers\InputController@yoyakudel");
+Route::middleware(['auth:sanctum', 'verified'])->any('/i/{miseId}/{therapistId}/{id}/yoyakuedit', "App\Http\Controllers\InputController@yoyakuedit");
 Route::middleware(['auth:sanctum', 'verified'])->any('/i/{miseId}/{therapistId}/{id}/yoyakuencho', "App\Http\Controllers\InputController@yoyakuencho");
 
 //client
