729a151d1146337c644c56e4afa3750f97df0b5e
ogura <harley0.love@gmail.com>
2023-01-11 17:31:08 +0900

    '2023/01/11/ogura'

--- public/js/shifuto.js
+++ public/js/shifuto.js
@@ -1,52 +1,109 @@
 $(function() {
 
-    //インプット要素を取得する
-    var inputs = $('.shiftTime');
+    // キーボードを押したとき
+    $(document).on("keydown", ".shiftTime", function(e){
 
-    // 入力したinoutのnameを取得
-    var name = inputs.attr('name')
+        var index       =   null;
+        var selector    =   ".shiftTime";
+        var time = $(this).val();
 
-    // class「shiftTime」に入力した時
-    $(inputs).on('input', function() {
+        // 「shift」を押しながら矢印でフォーカス移動
+        if(e.shiftKey){
+          if(e.keyCode === 37){
+            index   =   $(selector).index(this);
+            if (index > 0){
+                $(selector).eq(index-1).focus();
+            }
+            console.log('shift left');
+            return;
+          }
 
-        // 入力文字
-        $time = $(this).val();
-        // alert($time.slice(3,5));
+          if(e.keyCode === 38){
+            index   =   $(selector).index(this);
+            if (index > 0){
+                console.log(index);
+                if(index > 10){
+                    $(selector).eq(index-10).focus();
+                }
+            }
+            return;
+          }
 
-        // 入力された値が「.」であるか
-        if($(this).val().slice(-1) === '.'){
+          if(e.keyCode === 39){
+            index   =   $(selector).index(this);
+            if (index < $(selector).length - 1 ){
+                $(selector).eq(index+1).focus();
+            }
+            return;
+          }
 
-            // 開始時間で「.」を打った時
-            if($time.length <= 3){
+          if(e.keyCode === 40){
+            index   =   $(selector).index(this);
+            if (index < $(selector).length - 1 ){
+                $(selector).eq(index+10).focus();
+            }
+            return;
+          }
+        }   
+    });
+
+    // キーボードを押して離したとき
+    $(document).on("keyup", ".shiftTime", function(e){
+
+        var time = $(this).val();
+
+        // 「.」の場合
+        if( e.keyCode == 110 || e.keyCode == 190){
+            // 開始時間(区切り文字「-」がない)で「.」を打った時
+            if(time.length <= 3){
                 if($(this).val().includes('-') === false){
-                    $(this).val($time + "5-");
+                    $(this).val(time + "5-");
                 }
             }
             
-            // 終了時間で「.」を打った時
-            if($time.length > 3){
+            // 終了時間(区切り文字「-」がある)で「.」を打った時
+            if(time.length > 3){
                 if($(this).val().includes('-') != false){
-                    $(this).val($time + "5");
+                    $(this).val(time + "5");
                 }
             }
         }
 
-        // 「30分刻み(.5)がない場合」
+        // 「.」が無い場合
         if($(this).val().includes('.') === false){
-
-            if($time.length === 3){
-                if($check = $time.slice(0,1) < 3){
-                    $start = $time.slice(0,2);
-                    $end = $time.slice(2,5);
-                    $edit = $start + '-' + $end;
-                    $(this).val($edit);
+            if(time.length === 3){
+                if($check = time.slice(0,1) < 3){
+                    var start = time.slice(0,2);
+                    var end = time.slice(2,5);
+                    var edit = start + '-' + end;
+                    $(this).val(edit);
                 }else{
-                    $start = $time.slice(0,1);
-                    $end = $time.slice(1,5);
-                    $edit = $start + '-' + $end;
-                    $(this).val($edit);
+                    var start = time.slice(0,1);
+                    var end = time.slice(1,5);
+                    var edit = start + '-' + end;
+                    $(this).val(edit);
                 }
             }
         }
+
+        // ハイフン自動挿入回避
+        if($(this).val().includes('--') != false){
+            var tmp = $(this).val().split('--');
+            $(this).val(tmp[0] + '-')
+        }
+
+        // 編集時ハイフン回避
+        if(e.keyCode == 8 || e.keyCode == 46){
+            $(this).val(time);
+        }
+
+        // if(!(e.keyCode <= 105 && e.keyCode >= 96 ||
+        //         e.keyCode <= 57 && e.keyCode >= 48 || 
+        //         e.keyCode === 110 ||
+        //         e.keyCode === 190)){
+        //     console.log(time);
+        //     $(this).val(time.slice(0,-1));
+        //     return;
+        // }
     });
 });
